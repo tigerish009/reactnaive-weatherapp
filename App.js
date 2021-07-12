224a54230980d41ceceae8bb1883145a0c7e031d
tigerish009 <tigerish009@gmail.com>
2021-07-12 10:28:16 +0900

    make Loading Screen

--- App.js
+++ App.js
@@ -1,27 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}><Text >Hello!!!</Text></View>
-      <View style={styles.blueView}><Text >Hello!!!</Text></View>
-      <StatusBar style="auto" />
-    </View>
+    <Loading></Loading>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow",
-  },
-  blueView: {
-    flex: 3,
-    backgroundColor: "blue",
-  }
-});
